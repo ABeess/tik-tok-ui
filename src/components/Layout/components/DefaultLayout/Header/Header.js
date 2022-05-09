e5530d93a3ecc7cf8babba84b6eac57a365c8922
Abees <kurotoxic2510@gmail.com>
2022-05-10 00:38:39 +0700

    header ui

--- src/components/Layout/components/DefaultLayout/Header/Header.js
+++ src/components/Layout/components/DefaultLayout/Header/Header.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import Styles from './Header.module.scss'
+import images from '@/assets/images'
+// React Icon
+import { FiSearch } from 'react-icons/fi'
+import { AiFillCloseCircle, AiOutlineLoading3Quarters } from 'react-icons/ai'
 const cx = classNames.bind(Styles)
-
 const Header = () => {
 	return (
 		<header className={cx('wrapper')}>
-			<div className={cx('content')}></div>
+			<div className={cx('content')}>
+				<div className={cx('logo')}>
+					<img src={images.logo} alt="TikTok" />
+				</div>
+				<div className={cx('search')}>
+					<input
+						className={cx('search-input')}
+						placeholder="Tìm kiếm tài khoản và video"
+						spellcheck={false}
+					/>
+					<button className={cx('clear-btn')}>
+						<AiFillCloseCircle />
+					</button>
+					<AiOutlineLoading3Quarters className={cx('spinner')} />
+					<button className={cx('search-btn')}>
+						<FiSearch />
+					</button>
+				</div>
+				<div className={cx('actions')}></div>
+			</div>
 		</header>
 	)
 }
