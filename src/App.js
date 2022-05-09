ad0d3cd29369a6a23b01a79c0cfd19bdedbf7a80
Abees <kurotoxic2510@gmail.com>
2022-05-09 21:00:10 +0700

    style & variable color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import '@/App.css'
-
 function App() {
-	return <div className="App">Hello tik tok ui</div>
+	return (
+		<div className="App">
+			<h1>Hello tik tok ui</h1>
+		</div>
+	)
 }
 
 export default App
